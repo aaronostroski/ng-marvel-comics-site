41e7f11cc0b32432b806e7f8a2951f37d9c47e4f
aaronostroski <aaronostroski@gmail.com>
2019-04-22 01:33:47 -0300

    Criando card component e adicionando css.

---
+++ src/app/card/card.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-card',
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.css']
+})
+export class CardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
